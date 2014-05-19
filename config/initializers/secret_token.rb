c193f8c22a81a9114021eb3500246a457cdd572c
root <root@localhost.localdomain>
2014-05-19 15:36:48 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviceAdminTemplate::Application.config.secret_key_base = 'e03e16c6be9fc05e31e5a1797fad3b5857e5575de312b2c811d4338bfe7d44310e8b7d4428cc1396dfd56598263a0d2b5b68c541cac9c691b100ec4c77cc49de'
